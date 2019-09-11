fdf033e7477ff383ede82d5d046ffaae62881152
dahobbs <dah3@hw.ac.uk>
2019-09-11 22:50:33 +0100

    adding a list comp and gradients

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,4 +1,20 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4)
+#place values on scatter chart
+xValues = list(range(1,1001))
+yValues = [x**2 for x in xValues]
+
+#size of dot 200
+plt.scatter(xValues,yValues,c=yValues,cmap=plt.cm.YlGnBu,edgecolor='none' ,s=40)
+
+#set chart title and label axes
+plt.title("square numbers", fontsize=24)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+plt.axis([0,1100,0,1100000])
+
+#set the size of tick labels
+plt.tick_params(axis='both', which='major', labelsize=14)
+
 plt.show()
