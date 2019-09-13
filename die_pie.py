f6e6bb2edd81c1795af7e1389c43ef1ed13da288
david <david@pop-os.localdomain>
2019-09-13 14:58:14 +0100

    using pygal to analyse dice rolls

---
+++ die_pie.py
@@ -0,0 +1,31 @@
+import pygal
+from die import Die
+
+#create a D6
+die = Die()
+
+#make some rolls and store the results in a list
+
+results=[]
+
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+#analyise the results
+frequencies =[]
+for value in range(1,die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+#visualise the results with pygal
+hist = pygal.Bar()
+
+hist.title = "Results of rolling a dice 1000 times"
+
+hist.x_labels =  [1,2,3,4,5,6]
+hist.x_title = "Result"
+hist.y_title = "Frequency of result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_vis.svg')
---
+++ die_visual.py
@@ -0,0 +1,20 @@
+from die import Die
+
+#create a D6
+die = Die()
+
+#make some rolls and store the results in a list
+
+results=[]
+
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+#analyise the results
+frequencies =[]
+for value in range(1,die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
